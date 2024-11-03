03180060d817864a5ae2b9894827e9cd80dd5ef5
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-04 01:14:24 +0200

    update base url

--- src/app/apis/baseUrl.js
+++ src/app/apis/baseUrl.js
@@ -1,6 +1,6 @@
 //:TODO We will change base url value to life url value.
 
 // const BASE_URL = "http://localhost:5000/";
-const BASE_URL = `https://glitch.com/edit/#!/tungsten-shell-weaver`;
-// const BASE_URL = `https://tungsten-shell-weaver.glitch.me`;
+// const BASE_URL = `https://glitch.com/edit/#!/tungsten-shell-weaver`;
+const BASE_URL = `ihtc-backend-l3zyjgme7-hosnyabdelfatahs-projects.vercel.app`;
 export default BASE_URL;
