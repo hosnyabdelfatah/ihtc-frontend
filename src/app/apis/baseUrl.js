a160ac48ae2f34ad772c1a52af4e8c39b39be70a
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-05 13:52:05 +0200

    Back to backend url

--- src/app/apis/baseUrl.js
+++ src/app/apis/baseUrl.js
@@ -1,8 +1,9 @@
 //:TODO We will change base url value to life url value.
 
 // const BASE_URL = "http://localhost:5000/";
-// const BASE_URL = "https://ihtc-backend.vercel.app";
-const BASE_URL = "https://ihtc-backend-bp7f72ddb-hosnyabdelfatahs-projects.vercel.app";
+const BASE_URL = "https://ihtc-backend.vercel.app";
+// const BASE_URL = "https://ihtc-backend-bp7f72ddb-hosnyabdelfatahs-projects.vercel.app";
+
 // const BASE_URL = "https://ihtc-backend-97bpvpqe8-hosnyabdelfatahs-projects.vercel.app/";
 // const BASE_URL = `https://glitch.com/edit/#!/tungsten-shell-weaver`;
 // const BASE_URL = `https://ihtc-backend-gqnhcv11w-hosnyabdelfatahs-projects.vercel.app/`;
