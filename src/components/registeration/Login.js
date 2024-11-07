216e3c15fdaa8a1fb662ed798abd68bb13c43cff
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-07 14:48:23 +0200

    Update login component if user state is user or doctor

--- src/components/registeration/Login.js
+++ src/components/registeration/Login.js
@@ -72,6 +72,10 @@ const Login = ({}) => {
             setErrMsg('User and password is require');
             setLogging(false)
             return
+        } else if (userState === 'doctor' || userState === 'user') {
+            setErrMsg(`No server for ${userState} yet`)
+            setLogging(false)
+            return
         }
         try {
             if (userState === 'organization') {
@@ -88,8 +92,6 @@ const Login = ({}) => {
                 setUser('')
                 setPassword('')
                 navigate('/organization')
-            } else {
-                console.log(`No server for ${userState} yet`)
             }
 
         } catch (err) {
@@ -116,7 +118,7 @@ const Login = ({}) => {
              fill="none"
              viewBox="0 0 24 24">
         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                stroke-width="4"></circle>
+                strokewidth="4"></circle>
         <path className="opacity-75" fill="currentColor"
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
@@ -205,6 +207,7 @@ const Login = ({}) => {
                             handleLogging()
                         }}
                     >
+
                         {logging ? circleSpinner : 'Login'}
                     </button>
                 </form>
