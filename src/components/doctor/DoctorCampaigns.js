58fafbba585c81f6cdff000212915458ffc58031
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-28 06:00:03 +0200

    Doctor campaign under construction!

--- src/components/doctor/DoctorCampaigns.js
+++ src/components/doctor/DoctorCampaigns.js
@@ -187,148 +187,149 @@ const DoctorCampaigns = () => {
     return (
         <div className="w-[90%] mx-auto relative max-h-screen flex flex-col mb-8">
 
-            <div
-                className="search-bar  w-full min-h-8 flex flex-row justify-between items-center ">
-                <div className="selected-country_container relative w-[30%]
-                p-2 outline-none active:outline-none flex flex-row items-center
-                ">
-                    <span className="relative mr-3"
-                          onClick={() => {
-                              handleCountriesSelectShow()
-                              handleCountriesClicked()
-                          }}>
+            {/*<div*/}
+            {/*    className="search-bar  w-full min-h-8 flex flex-row justify-between items-center ">*/}
+            {/*    <div className="selected-country_container relative w-[30%]*/}
+            {/*    p-2 outline-none active:outline-none flex flex-row items-center*/}
+            {/*    ">*/}
+            {/*        <span className="relative mr-3"*/}
+            {/*              onClick={() => {*/}
+            {/*                  handleCountriesSelectShow()*/}
+            {/*                  handleCountriesClicked()*/}
+            {/*              }}>*/}
 
-                    <input type="text" className="selected-country border-2 z-10
-                    outline-none  text-center hover:cursor-pointer focus:outline-none text-indigo-500 font-semibold"
-                           value={selectedCountryText}
-                           placeholder="Select country"
-                           onChange={(e) => {
-                           }}
-                    />
+            {/*        <input type="text" className="selected-country border-2 z-10*/}
+            {/*        outline-none  text-center hover:cursor-pointer focus:outline-none text-indigo-500 font-semibold"*/}
+            {/*               value={selectedCountryText}*/}
+            {/*               placeholder="Select country"*/}
+            {/*               onChange={(e) => {*/}
+            {/*               }}*/}
+            {/*        />*/}
 
-                     <HiChevronDown
-                         className={`${countriesClicked ? "rotate-180" : "rotate-0"} cursor-pointer text-2xl  transition-all duration-400 block absolute top-[1%] right-1 z-10 `}
+            {/*         <HiChevronDown*/}
+            {/*             className={`${countriesClicked ? "rotate-180" : "rotate-0"} cursor-pointer text-2xl  transition-all duration-400 block absolute top-[1%] right-1 z-10 `}*/}
 
-                     />
-                    </span>
+            {/*         />*/}
+            {/*        </span>*/}
 
-                    <div className="search w-[3%] cursor-pointer bg-red ">
-                        <span className="search-icon text-xl  ">
-                            <IoSearchSharp className="text-red-600 block border-2"
-                                           onClick={() => {
-                                               handleSearchDoctors()
-                                               getAllDoctors()
-                                               handleClearInput()
-                                           }}/>
-                        </span>
-                    </div>
+            {/*        <div className="search w-[3%] cursor-pointer bg-red ">*/}
+            {/*            <span className="search-icon text-xl  ">*/}
+            {/*                <IoSearchSharp className="text-red-600 block border-2"*/}
+            {/*                               onClick={() => {*/}
+            {/*                                   handleSearchDoctors()*/}
+            {/*                                   getAllDoctors()*/}
+            {/*                                   handleClearInput()*/}
+            {/*                               }}/>*/}
+            {/*            </span>*/}
+            {/*        </div>*/}
 
-                    <ul className={`${countriesSelectShow ? "h-auto block" : " h-0 hidden"} countries absolute text-center list-none z-30  top-[36px] left-[8px]  w-[88%] cursor-pointer border overflow-y-scroll  bg-stone-100 transition-all
-                    `}>
-                        {
-                            loading ? <Skeleton className="h-8 w-30" times={8}/> : countryContent
-                        }
+            {/*        <ul className={`${countriesSelectShow ? "h-auto block" : " h-0 hidden"} countries absolute text-center list-none z-30  top-[36px] left-[8px]  w-[88%] cursor-pointer border overflow-y-scroll  bg-stone-100 transition-all*/}
+            {/*        `}>*/}
+            {/*            {*/}
+            {/*                loading ? <Skeleton className="h-8 w-30" times={8}/> : countryContent*/}
+            {/*            }*/}
 
-                    </ul>
+            {/*        </ul>*/}
 
-                </div>
-                <div className="search-data flex flex-row justify-between   border-2 border-stone-100 px-2 w-[69%]">
-                    <div>
-                        <span
-                            className="text-indigo-500 font-semibold mr-2">
-                            {Object.keys(allSelectedDoctors).length}
-                        </span>
-                        <span>doctors selected</span>
-                    </div>
-                    <div>
-                        <span>result</span>
-                        <span className="text-indigo-500 font-semibold"> {doctors.length} </span>doctors
-                    </div>
-                </div>
-            </div>
+            {/*    </div>*/}
+            {/*    <div className="search-data flex flex-row justify-between   border-2 border-stone-100 px-2 w-[69%]">*/}
+            {/*        <div>*/}
+            {/*            <span*/}
+            {/*                className="text-indigo-500 font-semibold mr-2">*/}
+            {/*                {Object.keys(allSelectedDoctors).length}*/}
+            {/*            </span>*/}
+            {/*            <span>doctors selected</span>*/}
+            {/*        </div>*/}
+            {/*        <div>*/}
+            {/*            <span>result</span>*/}
+            {/*            <span className="text-indigo-500 font-semibold"> {doctors.length} </span>doctors*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
 
-            <div className="w-full max-h-[60%] flex flex-row justify-between items-stretch mt-4 v  overflow-y-hidden">
+            {/*<div className="w-full max-h-[60%] flex flex-row justify-between items-stretch mt-4 v  overflow-y-hidden">*/}
 
-                <div className="specialty-bar overflow-y-scroll  w-4/12">
-                    {/*<input type="text"*/}
-                    {/*    // onChange={(e) => handleSearch(e)}*/}
-                    {/*       className="border w-full rounded mb-2 text-base  outline-none focus:outline-none p-1"*/}
-                    {/*    // value={search}*/}
-                    {/*/>*/}
-                    <DoctorSpecialties sendParent={handleChildValue} clearInput={clearInput}/>
-                </div>
+            {/*    <div className="specialty-bar overflow-y-scroll  w-4/12">*/}
+            {/*        /!*<input type="text"*!/*/}
+            {/*        /!*    // onChange={(e) => handleSearch(e)}*!/*/}
+            {/*        /!*       className="border w-full rounded mb-2 text-base  outline-none focus:outline-none p-1"*!/*/}
+            {/*        /!*    // value={search}*!/*/}
+            {/*        /!*/>*!/*/}
+            {/*        <DoctorSpecialties sendParent={handleChildValue} clearInput={clearInput}/>*/}
+            {/*    </div>*/}
 
-                <div className="flex flex-col w-10/12 ml-2 overflow-y-scroll ">
-                    <div className="data-show flex flex-row flex-wrap justify-center  ml-3 pt-2 mr-2">
-                        {/*Pagination*/}
-                        <table className="w-full">
-                            <tbody className="w-full">
-                            <tr className="w-full flex justify-start items-baseline">
-                                <td className="mr-3">
-                                    <input type="checkbox" className=" w-[20px] h-[20px]"
-                                           checked={isSelectAll}
-                                           onChange={() => {
-                                               handleSelectAll();
-                                               handleSelectAllDoctors()
-                                           }}
-                                    />
-                                </td>
-                                <td>Select all</td>
-                                <td className="  w-[60%] flex flex-row justify-center">
-                                    <button onClick={handleShowModal}
-                                            className="block p-1 border border-amber-400
-                                            rounded-full text-violet-700 font-bold">
-                                        Create
-                                        campaign
-                                    </button>
-                                </td>
-                            </tr>
-                            {loading && <Skeleton className="h-8 w-30" times={20}/>}
-                            {error && <div>Error Loading</div>}
-                            {
-                                doctors.length > 0 ? doctors?.map((doctor, index) => {
-                                    return <tr key={doctor._id}
-                                               className="border-b-[1px] w-full flex flex-row justify-between items-center text-xs mb-2  ">
-                                        <td key={doctor._id} className="w-[5%] flex flex-row justify-start relative  ">
-                                            <input type="checkbox" value={doctor._id} id={doctor._id}
-                                                   className="w-[20px] h-[20px] "
-                                                // checked={!!checkedItems[doctor._id]}
-                                                   checked={!!checkedItems[doctor._id]}
-                                                // onClick={(e) => handleSelectedDoctor(e)}
-                                                   onChange={
-                                                       (e) => {
-                                                           handleCheckboxChange(doctor._id)
-                                                           handleSelectedDoctor(e)
-                                                       }
-                                                   }
-                                            />
-                                        </td>
-                                        <td className="w-[25%] ">{doctor.fname + "" + doctor.lname}</td>
-                                        <td className="w-[32%]">{doctor.specialty.title}</td>
-                                        <td className="w-[17%]">{doctor.country.title}</td>
-                                        <td className="w-[10%]">{doctor.language.title}</td>
-                                        <td className="w-[7%]">
-                                            <img src={doctor.image} className="w-10 h-10"
-                                                 alt={(doctor.fname + "" + doctor.lname).toUpperCase()}/>
-                                        </td>
-                                    </tr>
-                                }) : <div className="flex justify-center mt-8 font-semibold text-red-600">
-                                    No doctors found belong to your conditions.
-                                </div>
-                            }
+            {/*    <div className="flex flex-col w-10/12 ml-2 overflow-y-scroll ">*/}
+            {/*        <div className="data-show flex flex-row flex-wrap justify-center  ml-3 pt-2 mr-2">*/}
+            {/*            /!*Pagination*!/*/}
+            {/*            <table className="w-full">*/}
+            {/*                <tbody className="w-full">*/}
+            {/*                <tr className="w-full flex justify-start items-baseline">*/}
+            {/*                    <td className="mr-3">*/}
+            {/*                        <input type="checkbox" className=" w-[20px] h-[20px]"*/}
+            {/*                               checked={isSelectAll}*/}
+            {/*                               onChange={() => {*/}
+            {/*                                   handleSelectAll();*/}
+            {/*                                   handleSelectAllDoctors()*/}
+            {/*                               }}*/}
+            {/*                        />*/}
+            {/*                    </td>*/}
+            {/*                    <td>Select all</td>*/}
+            {/*                    <td className="  w-[60%] flex flex-row justify-center">*/}
+            {/*                        <button onClick={handleShowModal}*/}
+            {/*                                className="block p-1 border border-amber-400*/}
+            {/*                                rounded-full text-violet-700 font-bold">*/}
+            {/*                            Create*/}
+            {/*                            campaign*/}
+            {/*                        </button>*/}
+            {/*                    </td>*/}
+            {/*                </tr>*/}
+            {/*                {loading && <Skeleton className="h-8 w-30" times={20}/>}*/}
+            {/*                {error && <div>Error Loading</div>}*/}
+            {/*                {*/}
+            {/*                    doctors.length > 0 ? doctors?.map((doctor, index) => {*/}
+            {/*                        return <tr key={doctor._id}*/}
+            {/*                                   className="border-b-[1px] w-full flex flex-row justify-between items-center text-xs mb-2  ">*/}
+            {/*                            <td key={doctor._id} className="w-[5%] flex flex-row justify-start relative  ">*/}
+            {/*                                <input type="checkbox" value={doctor._id} id={doctor._id}*/}
+            {/*                                       className="w-[20px] h-[20px] "*/}
+            {/*                                    // checked={!!checkedItems[doctor._id]}*/}
+            {/*                                       checked={!!checkedItems[doctor._id]}*/}
+            {/*                                    // onClick={(e) => handleSelectedDoctor(e)}*/}
+            {/*                                       onChange={*/}
+            {/*                                           (e) => {*/}
+            {/*                                               handleCheckboxChange(doctor._id)*/}
+            {/*                                               handleSelectedDoctor(e)*/}
+            {/*                                           }*/}
+            {/*                                       }*/}
+            {/*                                />*/}
+            {/*                            </td>*/}
+            {/*                            <td className="w-[25%] ">{doctor.fname + "" + doctor.lname}</td>*/}
+            {/*                            <td className="w-[32%]">{doctor.specialty.title}</td>*/}
+            {/*                            <td className="w-[17%]">{doctor.country.title}</td>*/}
+            {/*                            <td className="w-[10%]">{doctor.language.title}</td>*/}
+            {/*                            <td className="w-[7%]">*/}
+            {/*                                <img src={doctor.image} className="w-10 h-10"*/}
+            {/*                                     alt={(doctor.fname + "" + doctor.lname).toUpperCase()}/>*/}
+            {/*                            </td>*/}
+            {/*                        </tr>*/}
+            {/*                    }) : <div className="flex justify-center mt-8 font-semibold text-red-600">*/}
+            {/*                        No doctors found belong to your conditions.*/}
+            {/*                    </div>*/}
+            {/*                }*/}
 
-                            </tbody>
-                        </table>
-                        {doctors?.length > 0 && <Pagination currentPage={currentPage} totalPages={totalPages}
-                                                            onPageChange={setCurrentPage}/>}
+            {/*                </tbody>*/}
+            {/*            </table>*/}
+            {/*            {doctors?.length > 0 && <Pagination currentPage={currentPage} totalPages={totalPages}*/}
+            {/*                                                onPageChange={setCurrentPage}/>}*/}
 
-                        <div/>
-                        {/*End Pagination*/}
-                    </div>
+            {/*            <div/>*/}
+            {/*            /!*End Pagination*!/*/}
+            {/*        </div>*/}
 
-                </div>
-            </div>
-            {showModal && <Modal receivers={[...allSelectedDoctors]} onClose={handleCloseModal}/>}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*{showModal && <Modal receivers={[...allSelectedDoctors]} onClose={handleCloseModal}/>}*/}
+            Under Construction!
         </div>
     );
 }
