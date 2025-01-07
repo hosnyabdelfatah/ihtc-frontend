dc2f5ae8c1fec7038f2e9ae9435fd23374ce27e3
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2025-01-07 08:07:14 +0200

    Test organization page 3

--- src/components/organization/Organization.js
+++ src/components/organization/Organization.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function Organization(props) {
+    useEffect(() => {
+        console.log("Hello")
+    }, [])
     return (
         <div>Organization</div>
     );
